b190d95b266681668e94c39142a0c1d348f88a99
= <cabkebe02@gmail.com>
2024-04-27 00:13:50 +0000

    API for movies service

---
+++ routes/movies.routes.js
@@ -0,0 +1,17 @@
+
+module.exports = app =>{
+    let router = require("express").Router();
+    const movies = require("../Controller/movies.controller.js")
+    //expose API
+    app.use("/movie", router);
+
+    //GetAll movies
+    router.get("/all", movies.findAll);
+    //Find One movie
+    router.get("/:id", movies.findOne)
+    //Create a new movie
+    router.post("/", movies.create);
+
+    // Update a Student with id
+    router.put("/:id", movies.update);
+}
